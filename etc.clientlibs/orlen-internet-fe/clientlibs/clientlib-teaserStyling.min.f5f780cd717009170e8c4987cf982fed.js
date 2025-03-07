0a593919ed459c83e7458d056ded812c18d25f14
Markl1n1 <151952963+Markl1n1@users.noreply.github.com>
2025-03-08 01:37:09 +0200

    =

---
+++ etc.clientlibs/orlen-internet-fe/clientlibs/clientlib-teaserStyling.min.f5f780cd717009170e8c4987cf982fed.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".cmp-teaser__action-link");
+  buttons.forEach(element => {
+    const styling = element.getAttribute("styling");
+    const newLine = element.getAttribute("newLine");
+    if (newLine === 'true') {
+      element.style.display = 'table';
+    }
+    if (styling === "fullBrand") {
+      element.classList.add("fullBrand-styling", "common-styling", "common-styling-fullBrand");
+      element.classList.remove("cmp-teaser__action-link");
+    } else if (styling == "contourOnly") {
+      element.classList.add("contourOnly-styling", "common-styling");
+      element.classList.remove("cmp-teaser__action-link");
+    } else if (styling == "whiteBackground") {
+      element.classList.add("whiteBackground-styling", "common-styling");
+      element.classList.remove("cmp-teaser__action-link");
+    } else if (styling == "brandArrow") {
+      element.classList.add("brandArrow-styling", "common-styling-text");
+      element.classList.remove("cmp-teaser__action-link");
+    } else {
+      element.style.marginTop = '5px';
+      element.style.marginBottom = '5px';
+    }
+  });
+});
+
+
