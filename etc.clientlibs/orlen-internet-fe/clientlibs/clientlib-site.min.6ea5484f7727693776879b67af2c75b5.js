9dd3d1d453a4e97f9ff13d201b1b2c75ec8691be
Markl1n1 <151952963+Markl1n1@users.noreply.github.com>
2025-03-15 05:45:00 +0200

    change to orlen-energy

--- etc.clientlibs/orlen-internet-fe/clientlibs/clientlib-site.min.6ea5484f7727693776879b67af2c75b5.js
+++ etc.clientlibs/orlen-internet-fe/clientlibs/clientlib-site.min.6ea5484f7727693776879b67af2c75b5.js
@@ -83383,7 +83383,7 @@
                   "' fill-rule='nonzero'%3E%3Cpath d='M21.35 42V30.75h3v4.15H42v3H24.35V42ZM6 37.9v-3h12.35v3Zm9.35-8.3v-4.1H6v-3h9.35v-4.2h3v11.3Zm6-4.1v-3H42v3Zm8.3-8.25V6h3v4.1H42v3h-9.35v4.15ZM6 13.1v-3h20.65v3Z'/%3E%3C/g%3E%3C/svg%3E)"
                 ),
             PIE_CHART_LOGO:
-              "https://orlen.ltd/etc.clientlibs/orlen-internet-fe/clientlibs/clientlib-site/resources/images/orlen_chart_logo.png",
+              "https://orlen-energy.pl/etc.clientlibs/orlen-internet-fe/clientlibs/clientlib-site/resources/images/orlen_chart_logo.png",
             COLORS_ARRAY_LINECHART: [
               globalThis.GLOBAL_MAIN_COLOR,
               "#7cb5ec",
